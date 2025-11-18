91a5dd40ddd903e546ef7a4844b77a4d47e2d9b0
cheru <30118310+DennoCheru@users.noreply.github.com>
2025-11-18 23:58:26 +0300

    Create Gameboard module

---
+++ script.js
@@ -0,0 +1,24 @@
+const Gameboard = (function() {
+    board = Array(9).fill("");
+
+    const getBoard = () => [...board];
+
+    const setMove = (mark, index) => {
+        if (board[index] != "") {
+            board[index] = mark;
+            return true;
+        }
+        return false;
+    }
+    const resetBoard = () => {
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+        }
+    }
+    return {
+        getBoard,
+        setMove,
+        resetBoard,
+    }
+
+})();
